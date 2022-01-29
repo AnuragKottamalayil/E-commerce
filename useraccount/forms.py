4b4858966f060b832c4b492cbd48cd16b9aadf08
Anurag <anurag@evoqueinnovativelab.com>
2022-01-29 23:31:01 +0530

    login, register design fixed

--- useraccount/forms.py
+++ useraccount/forms.py
@@ -9,14 +9,65 @@ class RegisterForm(forms.ModelForm):
         model = LoginTable
         fields = ('first_name','last_name','username','email','phone_no','password','address','zipcode')
         help_texts = {'username':None}
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+            'phone_no': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone No'}),
+            'password': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}),
+            'zipcode': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Zipcode'})
+        }
+
+        labels = {
+            "first_name": "",
+            "last_name": "",
+            "username": "",
+            "email": "",
+            "phone_no": "",
+            "password": "",
+            "address": "",
+            "zipcode": "",
+        }
 
 class LoginForm(forms.ModelForm):
     class Meta:
         model = LoginTable
         fields = ('username','password')
+        help_texts = {'username':None}
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'password': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+        }
+        labels = {
+            "username": "",
+            "password": "",
+        }
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = LoginTable
         fields = ('first_name','last_name','username','email','phone_no','address','zipcode')
         help_texts = {'username':None}
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+            'phone_no': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone No'}),
+            'password': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}),
+            'zipcode': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Zipcode'})
+        }
+
+        labels = {
+            "first_name": "",
+            "last_name": "",
+            "username": "",
+            "email": "",
+            "phone_no": "",
+            "password": "",
+            "address": "",
+            "zipcode": "",
+        }
